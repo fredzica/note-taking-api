2f84f7a5e9dcf3ffb345225908ac10242c347f86
Frederico Zica <fredzica@gmail.com>
2022-08-02 18:55:40 -0300

    POST /notes test

--- test/controllers/notes.test.ts
+++ test/controllers/notes.test.ts
@@ -2,12 +2,14 @@ import notes from '../../src/controllers/notes'
 
 import express from 'express'
 import request from 'supertest'
+import bodyParser from 'body-parser'
 
 import { deleteAllTables, initDb } from '../../src/services/db'
-import { createNote, findNote } from '../../src/db/note'
+import { createNote, findNote, findUserNotes } from '../../src/db/note'
 import NoteDTO from '../../src/dto/NoteDTO'
 
 const app = express()
+app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/notes', notes)
 
@@ -43,12 +45,32 @@ describe('Notes router', () => {
       .get('/notes')
       .set('Accept', 'application/json')
 
-    console.log(response.body)
     expect(response.headers['content-type']).toMatch(/json/)
     expect(response.status).toEqual(200)
 
     expect(response.body).toContainEqual<NoteDTO>(note1)
     expect(response.body).toContainEqual<NoteDTO>(note2)
     expect(response.body).toHaveLength(2)
+  })
+
+  it('POST /notes, missing notes field', async () => {
+    await request(app)
+      .post('/notes')
+      .send({ wrong: 'element' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+  })
+
+  it('POST /notes, creates a note', async () => {
+    const response = await request(app)
+      .post('/notes')
+      .send({ note: 'a simple note' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+
+    const notes = findUserNotes(1)
+    expect(response.body).toEqual<NoteDTO>(notes[0])
   })
 })
