6ca66c5356d73752f1a9fc7aeccb597085dd15f6
Frederico Zica <fredzica@gmail.com>
2022-08-02 18:55:58 -0300

    POST /notes implementation

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -1,14 +1,27 @@
 import express from 'express'
-import { createNote, findUserNotes } from '../db/note'
+import { createNote, findNote, findUserNotes } from '../db/note'
 import { verifyAuth } from '../middleware/auth'
 
 const router = express.Router()
 
-router.get('/', verifyAuth, (req, res) => {
+router.get('/', verifyAuth, async (req, res) => {
   const userId = res.locals.user.id
   const userNotes = findUserNotes(userId)
 
   res.send(userNotes)
+})
+
+router.post('/', verifyAuth, async (req, res) => {
+  const note = req.body.note
+  if (!note) {
+    res.status(400).send({ error: "The 'note' field is missing" })
+    return
+  }
+
+  const userId = res.locals.user.id
+  const noteId = createNote(userId, note)
+  const createdNote = findNote(noteId)
+  res.status(201).send(createdNote)
 })
 
 export default router
