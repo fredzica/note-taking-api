98746b73d3ee78cb59df13cb9c5737b3b9f7601d
Frederico Zica <fredzica@gmail.com>
2022-08-02 12:13:46 -0300

    initialize the DB schema and first rows

--- src/services/db.ts
+++ src/services/db.ts
@@ -2,19 +2,48 @@ import sqlite3 from 'sqlite3'
 sqlite3.verbose()
 const db = new sqlite3.Database(':memory:')
 
+/**
+ * Creates the DB tables and first rows.
+ */
 const initDb = async () => {
+  // ensures serial, in-order execution of SQL
   db.serialize(() => {
-    db.run('CREATE TABLE lorem (info TEXT)')
+    db.run(`
+    CREATE TABLE user(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP
+    )`)
 
-    const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-    for (let i = 0; i < 10; i++) {
-      stmt.run('Ipsum ' + i)
-    }
-    stmt.finalize()
+    db.run(`
+    CREATE TABLE note(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id TEXT NOT NULL,
+      note TEXT NOT NULL,
+      created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP,
+      FOREIGN KEY(user_id) REFERENCES user(id)
+    )`)
 
-    db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
-      console.log(row.id + ': ' + row.info)
-    })
+    db.run(`
+    INSERT INTO user(
+      username, password
+    )
+    VALUES(
+      'test1',
+      'pass'
+    )`)
+
+    db.run(`
+    INSERT INTO user(
+      username, password
+    )
+    VALUES(
+      'test2',
+      'pass'
+    )`)
   })
 }
 
